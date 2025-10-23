c9aa0a2f2b95e163da1427047507feceee3ee2ed
Ademola10 <ademolaogunmokun@yahoo.com>
2025-10-23 23:35:54 +0100

    feat: update assets and improve login screen layout with safe area insets

--- src/navigation/screens/Login.tsx
+++ src/navigation/screens/Login.tsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { theme } from '../../../config/theme'
-
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 const Login = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <View>
+    <View style={{ paddingTop: insets.top + theme.spacing.md }}>
       <Text>Login</Text>
     </View>
   )
